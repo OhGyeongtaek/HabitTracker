b69a124d2a817872ad3cd4c96478f596899eb5a0
OKT <rudxor97@naver.com>
2021-04-23 23:30:50 +0900

    해더와 습관 추가하는 기능 구현俔

---
+++ src/components/habitHeader.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class HabitHeader extends Component {
+    render() {
+        return (
+            <header>
+                <h1>로고</h1>
+                <h1>Habit Tracker</h1>
+                <strong>
+                    {this.props.count}
+                </strong>
+            </header>
+        );
+    }
+}
+export default HabitHeader;
